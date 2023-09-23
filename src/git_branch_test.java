7be070e7ba0ad4a98181fbaa6ee9ceb8901a5d17
‘lhm’ <1271880639@qq.com>
2023-09-23 15:00:22 +0800

    hot_fix第一次提交

--- src/git_branch_test.java
+++ src/git_branch_test.java
@@ -8,5 +8,6 @@ public class git_branch_test {
     public static void main(String[] args) {
         System.out.println("master第一次提交");
         System.out.println("master第二次提交");
+        System.out.println("hot_fix第一次修改");
     }
 }
