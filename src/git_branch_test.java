803c81c8ed1cd962607e5a34e1b47a99f5fc133c
‘lhm’ <1271880639@qq.com>
2023-09-23 15:24:11 +0800

    master做一次修改

--- src/git_branch_test.java
+++ src/git_branch_test.java
@@ -10,5 +10,6 @@ public class git_branch_test {
         System.out.println("master第二次提交");
         System.out.println("hot_fix第一次修改");
         System.out.println("hot_fix第二次修改");
+        System.out.println("master做一次修改");
     }
 }
